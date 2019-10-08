1b09054b1f2a93f738acfc0272fa2edee4031b01
bibi <bibi@DESKTOP-A0L7EOC>
2019-10-08 22:28:17 +0200

    Mise à jour des méthodes de la class Order!

--- src/com/ocr/anthony/Main.java
+++ src/com/ocr/anthony/Main.java
@@ -4,11 +4,11 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Order order = new Order();
-        order.displayAvailableMenu();
-        Scanner sc = new Scanner(System.in);
-        int nb = sc.nextInt();
-        order.displaySelectedMenu(nb);
-    }
+	public static void main(String[] args) {
+		Order order = new Order();
+		order.displayAvailableMenu();
+		Scanner sc = new Scanner(System.in);
+		int nb = sc.nextInt();
+		order.displaySelectedMenu(nb);
+	}
 }
--- test/com/ocr/anthony/OrderTest.java
+++ test/com/ocr/anthony/OrderTest.java
@@ -1,39 +1,39 @@
 package com.ocr.anthony;
 
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class OrderTest {
 
-    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 
-    @BeforeEach
-    public void setUpStreams() {
-        System.setOut(new PrintStream(outContent));
-    }
+	@BeforeEach
+	public void setUpStreams() {
+		System.setOut(new PrintStream(outContent));
+	}
 
-    @AfterEach
-    public void restoreStreams() {
-        System.setOut(System.out);
-    }
+	@AfterEach
+	public void restoreStreams() {
+		System.setOut(System.out);
+	}
 
-    Order order = new Order();
+	Order order = new Order();
 
-    @Test
-    public void Given_Nothing_When_DisplayMenuSelection_Then_ShouldDisplayText() {
-        order.displayAvailableMenu();
-        assertEquals(false, outContent.toString().isEmpty());
-    }
-    @Test
-    public void Given_Vegetarian_When_AskForMenuSelected_Then_ShouldDisplayCorrectSentence() {
-        order.displaySelectedMenu(3);
-        assertEquals("Vous avez choisi le menu 3\n", outContent.toString().replace("\r\n", "\n"));
-    }
+	@Test
+	public void Given_Nothing_When_DisplayMenuSelection_Then_ShouldDisplayText() {
+		order.displayAvailableMenu();
+		assertEquals(false, outContent.toString().isEmpty());
+	}
+
+	@Test
+	public void Given_Vegetarian_When_AskForMenuSelected_Then_ShouldDisplayCorrectSentence() {
+		order.displaySelectedMenu(3);
+		assertEquals("Vous avez choisi le menu 3\n", outContent.toString().replace("\r\n", "\n"));
+	}
 }
